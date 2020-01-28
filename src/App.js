0e70b0542b4ded258f12795a114255ae77e18205
hikaru <n19004@std.it-college.ac.jp>
2020-01-28 15:01:39 +0900

    授業

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CBoxForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { check: true }
+  }
+
+render () {
+  return (<div>
+      <form onSubmit={e => this.doSubmit(e)}>
+          <label>
+              <input type='checkbox'
+                  onChange={e => this.doChange(e)}
+                  checked={this.state.check}
+              </label><br />
+              <input type='submit' value='決定' />
+          </form>
+      </div>)
+    }
+    doChange (e) {
+        this.setState({ check: !this.state.check })
+                  }
+    
+    doSubmit (e) {
+      e.preventDefault()
+      window.alert(this.state.check ? '食べる' : '食べない')
+    }
 }
 
+ReactDom.render(
+  <CBoxForm />
+  document.getElementById('root')
+)
+                  
 export default App;

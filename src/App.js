bdbd159a24de363f58f99df125cadf60f0486b0b
hikaru <n19004@std.it-college.ac.jp>
2020-01-30 14:11:23 +0900

    授業

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import jsonp from 'fetch-jsonp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getJSON = async uri => {
+  const result = await fetch(uri).then(res => res.json)
+  return result
 }
 
-export default App;
+export class SimpleForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: {} }
+  }
+
+  componentDidMount () {
+    getJSON().then(json => this.setState({ items: json }))
+  }
+
+  render () {
+    console.log(this.state)
+  }
+}
+export default App
